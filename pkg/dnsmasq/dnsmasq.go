07e2e01650e0f2940c8096006e1f4640726e351c
matheus <mkwinter@seashell.sh>
2020-08-20 16:03:18 -0300

    Update dnsmasq pkg

--- pkg/dnsmasq/dnsmasq.go
+++ pkg/dnsmasq/dnsmasq.go
@@ -1,32 +1,54 @@
 package dnsmasq
 
 import (
-	"os"
 	"os/exec"
+	"syscall"
+	"fmt"
+	"encoding/json"
 )
 
+type Config struct {
+	Address string
+	DhcpRange string
+	DhcpOption string
+	Iface string
+}
+
+const (
+	defaultAddress="/#/192.168.42.1"
+	defaultdhcpRange="192.168.42.2,192.168.42.254"
+	defaultdhcpOption="option:router,192.168.42.1"
+	defaultInterface="wlp2s0"
+)
+
+func PrettyPrint(v interface{}) (err error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err == nil {
+			  fmt.Println(string(b))
+	}
+	return
+}
 
 // StartDnsmasq :
-func StartDnsmasq() (error) {
-	
-	cmd := exec.Command("dnsmasq",
-								"--address=/#/192.168.42.1",
-								"--dhcp-range=192.168.42.2,192.168.42.254",
-								"--dhcp-option=option:router,192.168.42.1",
-								"--interface=wlp2s0",
-								"--keep-in-foreground",
-								"--bind-interfaces",
-								"--except-interface=lo",
-								"--conf-file",
-								"--no-hosts",
-							)
-
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	if err != nil {
-		return err
+func StartDnsmasq(c *Config) (error) {
+	PrettyPrint(c)
+	go func(){
+		cmd := exec.Command("dnsmasq",
+		"--address=" + c.Address,
+		"--dhcp-range=" + c.DhcpRange,
+		"--dhcp-option=" + c.DhcpOption,
+		"--interface=" + c.Iface,
+		"--keep-in-foreground",
+		"--bind-interfaces",
+		"--except-interface=lo",
+		"--no-hosts",
+	)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Pdeathsig: syscall.SIGTERM,
 	}
 
-	return nil
+	cmd.Start()
+  }()
+	
+	return  nil
 }
