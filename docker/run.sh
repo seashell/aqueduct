62a65ce4deb647d5521dd0573a7ff299f2fb678c
matheus <mkwinter@seashell.sh>
2020-08-20 16:03:00 -0300

    Update docker related files

--- docker/run.sh
+++ docker/run.sh
@@ -4,9 +4,11 @@ docker pull seashelltechnology/aqueduct
 
 docker run --rm -ti \
       --name aqueduct \
+      --env HOTSPOT_SSID="$(hostname)" \
       --env DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS" \
       --volume /run/user/1000/bus:/run/user/1000/bus \
       --volume /run/dbus/system_bus_socket:/run/dbus/system_bus_socket \
       --network host \
       --cap-add NET_ADMIN \
       seashelltechnology/aqueduct
+      
